7906d8f4b089cfc309e812a6d12f7509f8e86597
AlbertSoonJa <y95sm95@gmail.com>
2024-12-19 13:49:01 +0900

    Add files via upload
    first commit

---
+++ src/test/java/qna/qna_spring/QnaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package qna.qna_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QnaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
